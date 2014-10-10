043ea90264253b5e42016a9a537a6a9084a7dc20
Erwin Dupont <erwin.dupont@hp.com>
2014-10-10 14:53:56 +0200

    Added dependency to Joda Beans.

---
+++ src/main/java/edu/tryouts/jodabeans/JodaBeans.java
@@ -0,0 +1,19 @@
+package edu.tryouts.jodabeans;
+
+/**
+ * Class JodaBeans.
+ *
+ * @author Erwin Dupont
+ * @since 2014-10-10
+ */
+public class JodaBeans {
+	/**
+	 * Command line application execution.
+	 *
+	 * @param args Array of Strings referencing the command line arguments.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
