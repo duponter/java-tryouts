c20033d979d9302223e22880ee507608a18f5f13
Erwin Dupont <erwin.dupont@hp.com>
2014-10-14 11:24:25 +0200

    Added JUnit TheoriesExample.

---
+++ src/main/java/edu/tryouts/junit/Soul.java
@@ -0,0 +1,21 @@
+package edu.tryouts.junit;
+
+import edu.tryouts.java8.Named;
+
+/**
+ * Class Soul.
+ *
+ * @author Erwin Dupont
+ * @since 2014-10-14
+ */
+public class Soul {
+	private Named owner;
+
+	public Named getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Named owner) {
+		this.owner = owner;
+	}
+}
---
+++ src/test/java/edu/tryouts/junit/TheoriesExample.java
@@ -0,0 +1,49 @@
+package edu.tryouts.junit;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+/**
+ * Class TheoriesExample.
+ *
+ * @author Erwin Dupont
+ * @since 2014-10-14
+ */
+@RunWith(Theories.class)
+public class TheoriesExample {
+	/*
+	* 	Our test	data are	stored in	an array	of Oligarch	objects.
+	* 	Since this	is a	method like	any other, you	can generate	data
+	*	dynamically or	fetch it	from an	external source, if needed.
+	*/
+	@DataPoints
+	public static Oligarch[] data() {
+		return new Oligarch[]{
+				new Oligarch("Monty Burns"),
+				new Oligarch("Don Geiss"),
+				new Oligarch("Arthur Jensen")
+		};
+	}
+
+	/*
+	*	This theory	confirms that	all oligarchs	have yachts	.
+	*/
+	@Theory
+	public void theoryOligarchsHaveYachts(Oligarch suit) {
+		assertThat(suit.getVehicles(), hasItem("yacht"));
+	}
+
+	/*
+	*	This theory	confirms that	all oligarchs	have hearts	of darkest	black.
+	*/
+	@Theory
+	public void theoryOligarchsAreEvil(Oligarch suit) {
+		assertThat(suit.getSoul().getOwner().getName(), is("The Devil"));
+	}
+}
