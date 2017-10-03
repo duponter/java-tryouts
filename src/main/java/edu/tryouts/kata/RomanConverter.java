dcac3eb2723079128513d5c8dde4127ee9906e73
Erwin Dupont <erwin.dupont@aca-it.be>
2017-10-03 13:38:19 +0200

    Replaced static Map with Enum

--- src/main/java/edu/tryouts/kata/RomanConverter.java
+++ src/main/java/edu/tryouts/kata/RomanConverter.java
@@ -1,8 +1,5 @@
 package edu.tryouts.kata;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -12,35 +9,14 @@ import org.apache.commons.lang3.StringUtils;
  * @since 2015-05-20
  */
 public class RomanConverter {
-	public static Map<Integer, String> arabicsToRomans = new LinkedHashMap<>();
-
-	static {
-		arabicsToRomans.put(1000, "M");
-		arabicsToRomans.put(900, "CM");
-		arabicsToRomans.put(500, "D");
-		arabicsToRomans.put(400, "CD");
-		arabicsToRomans.put(100, "C");
-		arabicsToRomans.put(90, "XC");
-		arabicsToRomans.put(50, "L");
-		arabicsToRomans.put(40, "XL");
-		arabicsToRomans.put(10, "X");
-		arabicsToRomans.put(9, "IX");
-		arabicsToRomans.put(5, "V");
-		arabicsToRomans.put(4, "IV");
-		arabicsToRomans.put(1, "I");
-	}
-
-	;
-
 	public String convert(Integer number) {
 		String result = StringUtils.EMPTY;
 
-		for (Map.Entry<Integer, String> entry : arabicsToRomans.entrySet()) {
-			Integer arabicNumeral = entry.getKey();
-			String romanNumeral = entry.getValue();
+		for (RomanNumeral roman : RomanNumeral.values()) {
+			Integer arabicNumeral = roman.toArabicNumeral();
 
 			while (number >= arabicNumeral) {
-				result += romanNumeral;
+				result += roman.name();
 				number -= arabicNumeral;
 			}
 		}
